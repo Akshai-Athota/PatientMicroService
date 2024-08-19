5c283e5ef48852f9ed4f6a3913b11955fb90a2c5
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-08-19 21:15:52 +0530

    intial folder set up

---
+++ src/test/java/com/aksahi/patientMicroService/PatientMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksahi.patientMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
